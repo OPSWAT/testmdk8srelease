549bf7e37d6a7070a7f1dd51a87a2157eda37a93
Sebastian Chesa <sebastian.chesa@opswat.com>
2024-08-20 10:39:04 +0300

    MOPS-106 changed tc params

--- .tc_automation/release-bitbucket-metadefenderk8s.sh
+++ .tc_automation/release-bitbucket-metadefenderk8s.sh
@@ -1,5 +1,9 @@
+echo "### Reading Params"
+SSH_KEY=$1
+RELEASE_VERSION=$2
+
 echo "### Setting GitHub Key"
-echo "%SSH_KEY%" > ~/.ssh/gh_rsa
+echo "$SSH_KEY" > ~/.ssh/gh_rsa
 
 echo "### Scanning Bitbucket Code"
 cd bitbucket
@@ -15,14 +19,14 @@ docker run --rm \
     chmod 600 /root/.ssh/gh_rsa &&
     GIT_SSH_COMMAND='ssh -i /root/.ssh/gh_rsa -o StrictHostKeyChecking=no -o IdentitiesOnly=yes' git clone git@github.com:OPSWAT/metadefender-k8s-internal.git &&
     cd metadefender-k8s-internal &&
-    GIT_SSH_COMMAND='ssh -i /root/.ssh/gh_rsa -o IdentitiesOnly=yes' git checkout -b %RELEASE_VERSION% &&
+    GIT_SSH_COMMAND='ssh -i /root/.ssh/gh_rsa -o IdentitiesOnly=yes' git checkout -b $RELEASE_VERSION &&
     git config --global user.email 'buildautomation@opswat.com' &&
     git config --global user.name 'Build Automation' &&
     rm -r * &&
     cp -r /bitbucket/* ./ &&
     GIT_SSH_COMMAND='ssh -i /root/.ssh/gh_rsa -o IdentitiesOnly=yes' git add . &&
-    GIT_SSH_COMMAND='ssh -i /root/.ssh/gh_rsa -o IdentitiesOnly=yes' git commit -m 'Release version %RELEASE_VERSION%' &&
-    GIT_SSH_COMMAND='ssh -i /root/.ssh/gh_rsa -o StrictHostKeyChecking=no -o IdentitiesOnly=yes' git push origin %RELEASE_VERSION%
+    GIT_SSH_COMMAND='ssh -i /root/.ssh/gh_rsa -o IdentitiesOnly=yes' git commit -m 'Release version $RELEASE_VERSION' &&
+    GIT_SSH_COMMAND='ssh -i /root/.ssh/gh_rsa -o StrictHostKeyChecking=no -o IdentitiesOnly=yes' git push origin $RELEASE_VERSION
   "
 
 echo "### Removing ssh key"
