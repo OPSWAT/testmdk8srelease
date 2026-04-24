b0da7c54aafbceb50c55d3fb48884230fbf07f93
Pablo Hernandez <pablo.hernandez@opswat.com>
2026-04-24 13:49:10 +0200

    Delete tc automation

--- .tc_automation/gitleaks-scanning.sh
+++
@@ -1,6 +0,0 @@
-rm -rf gitleaksReports
-mkdir gitleaksReports
-chmod +777 gitleaksReports
-
-echo "Scanning started"
-docker run -v .:/path zricethezav/gitleaks:latest detect --source="/path" --no-git -c /path/.tc_automation/gitleaks.toml --redact -r /path/gitleaksReports/gitleaks.json
